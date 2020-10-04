fb626f37c079fe62f4da8610f7701c89827f35a1
jyj <1553448104@qq.com>
2020-10-04 09:42:04 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// 引入less
+import less from 'less'
+Vue.use(less)
+// 导入路由
+import router from './router/index.js'
 
+Vue.config.productionTip = false;
+import { Button } from 'vant';
+Vue.use(Button)
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
